e4610d8e8a441c27c84759ede27fafa8b97cc32a
dat-nguyen-304 <nguyenphatdat3004@gmail.com>
2023-02-04 10:47:04 +0700

    5 commit

--- src/index.js
+++ src/index.js
@@ -110,5 +110,5 @@ app.post("/customers/:username", (req, res) => {
 
 
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
